dae9801249dfd9161cac1fffde2477652a948de4
daniel9x <dsgrant@emory.edu>
2016-03-01 17:57:26 -0500

    Removing getters and setters

--- src/main/java/edu/emory/first/cache/model/User.java
+++ src/main/java/edu/emory/first/cache/model/User.java
@@ -43,19 +43,4 @@ public class User implements Serializable {
     @Column(name = "Email")
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
