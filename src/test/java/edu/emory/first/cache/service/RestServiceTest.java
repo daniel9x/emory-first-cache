798c29c95f3e91f76f2a079e860c6dbd97011841
daniel9x <dsgrant@emory.edu>
2016-03-31 10:04:23 -0400

    Adding tests

--- src/test/java/edu/emory/first/cache/service/RestServiceTest.java
+++ src/test/java/edu/emory/first/cache/service/RestServiceTest.java
@@ -1,5 +1,8 @@
 package edu.emory.first.cache.service;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -9,6 +12,8 @@ import edu.emory.first.cache.util.TestUtil;
 
 
 public class RestServiceTest {
+    
+    RestService restService = new RestService();
 	
 	@BeforeClass
 	public static void init() {
@@ -44,4 +49,48 @@ public class RestServiceTest {
     	pat.getHeadingPlural();
     }
     
+    @Test
+    public void testGetPublicationTypes() {
+    	restService.getPublicationTypes();
+    }
+    
+    @Test
+    public void testTest() {
+    	restService.getTest();
+    }
+    
+    @Test
+    public void testGetUserPublications() {
+    	restService.getUserPublications("","",true);
+    }
+    
+    @Test
+    public void testGetUserPublicationsWithNullValues() {
+    	restService.getUserPublications("",null,null);
+    }
+    
+    @Test
+    public void testGetUserProfessionalActivities() {
+    	restService.getUserProfessionalActivities("",0);
+    }
+    
+    @Test
+    public void testConvertBuildDateFormat() {
+    	restService.convertBuildDateFormat("12345");
+    }
+    
+    @Test
+    public void testGetUserProfessionalActivitiesWithNullTypeId() {
+    	restService.getUserProfessionalActivities("",null);
+    }
+    
+    @Test
+    public void testGetUser() {
+    	restService.getUser("");
+    }
+    
+    @Test
+    public void testGetBuildNumber() throws IOException, ParseException {
+    	restService.getBuildNumber();
+    }
 }
